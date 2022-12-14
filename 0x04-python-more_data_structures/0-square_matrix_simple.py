aeb48680e3bd7930ff04767fcc92999aee07f31a
Captressketh001 <shininglite2012@gmail.com>
2022-12-14 23:52:14 +0000

    task 0

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+	squared_matrix = []
+	for i in matrix:
+		squared_row = []
+		for x in i:
+			squared_row.append(x**2);
+		squared_matrix.append(squared_row)
+	return squared_matrix
