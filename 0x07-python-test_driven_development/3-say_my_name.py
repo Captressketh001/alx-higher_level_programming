3937040cc27161c110cd56909771ffdd0bc25178
Captressketh001 <shininglite2012@gmail.com>
2023-01-28 21:56:00 +0100

    update

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -14,9 +14,9 @@ def say_my_name(first_name, last_name=""):
     Prints:
          My name is <first_name> <last_name>
     """
-    if not (isinstance(first_name, str)):
+    if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-    if not (isinstance(last_name, str)):
+    if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
     if (first_name is None or last_name is None):
         pass
