1f1d800ed49a0a174c4d36e5419733811607a644
Captressketh001 <shininglite2012@gmail.com>
2023-03-15 10:52:50 +0000

    removed comparison

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
 
 const arg = process.argv;
-function compare (a, b) {
-  return b - a;
-}
 
 if (arg.length === 2) {
   console.log(0);
@@ -11,6 +8,6 @@ if (arg.length === 2) {
   console.log(0);
 } else {
   const index = arg.slice(2);
-  const value = index.sort(compare).reverse();
+  const value = index.sort().reverse();
   console.log(value[1]);
 }
