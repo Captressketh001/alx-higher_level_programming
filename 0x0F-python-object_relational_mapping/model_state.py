e402b80df3f4e7164d069cb312978ae1e6c553dd
Captressketh001 <shininglite2012@gmail.com>
2023-03-19 09:34:04 -0700

    Dfix

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -8,6 +8,10 @@ from sqlalchemy import Column, Integer, String
 Base = declarative_base()
 
 class State(Base):
+    """defines a class state that links to the states table
+    with class attribute id that represents a column of an auto-generated, 
+    unique integer, can’t be null and is a primary key
+    """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
