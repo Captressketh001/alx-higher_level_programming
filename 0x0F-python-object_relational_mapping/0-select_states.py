f67cfa6953b91ec7cb8d2801abbbb691a532c6bb
Captressketh001 <shininglite2012@gmail.com>
2023-03-20 09:30:43 -0700

    remove whitespace

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,10 +9,10 @@ from sys import argv
 if __name__ == "__main__":
 
     # Connecting to MySQL database
-    db = MySQLdb.connect(host="localhost", 
+    db = MySQLdb.connect(host="localhost",
                          port=3306,
-                         user=argv[1], 
-                         passwd=argv[2], 
+                         user=argv[1],
+                         passwd=argv[2],
                          db=argv[3])
 
     # Get a cursor
