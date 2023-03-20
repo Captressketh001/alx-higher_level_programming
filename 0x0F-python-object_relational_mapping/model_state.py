e1eb141c2b034d37eb9218a1c223aeefa4d1d022
Captressketh001 <shininglite2012@gmail.com>
2023-03-20 17:55:02 +0100

    fixed issues on file

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 
-"""
-Write a python file that contains the class definition of a State and an instance Base = declarative_base()
+"""Write a python file that contains the class definition 
+of a State and an instance Base = declarative_base()
 """
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 Base = declarative_base()
 
 class State(Base):
+
     """defines a class state that links to the states table
-    with class attribute id that represents a column of an auto-generated, 
+    with class attribute id that represents a column of an auto-generated,
     unique integer, can’t be null and is a primary key
     """
     __tablename__ = 'states'
