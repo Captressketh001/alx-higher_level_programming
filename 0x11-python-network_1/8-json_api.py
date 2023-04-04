3bc992ec18c0ab4ff758d9d946f14977c7eeb568
Oluwakemi Omoyeni <shininglite2012@gmail.com>
2023-04-04 16:39:09 +0100

    upddated the if statement for argument

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,7 +8,7 @@ from sys import argv
 
 if __name__ == '__main__':
     url = 'http://0.0.0.0:5000/search_user'
-    if len(argv == 2):
+    if len(argv) == 2:
         q = argv[1]
     else:
         q = ""
