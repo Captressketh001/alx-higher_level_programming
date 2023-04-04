977d7c15f2711a2d3b5c1340cabcae070db7dd6e
Oluwakemi Omoyeni <shininglite2012@gmail.com>
2023-04-04 13:21:36 +0100

    fixed pycode styke issue

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,13 +4,12 @@
 
 if __name__ == "__main__":
     import urllib.request
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status')\
-        as response:
-            html = response.read()
+    req = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
 
     decoded_html = html.decode('utf-8')
     print("Body response:")
     print(f"\t- type: {type(html)}")
     print(f"\t- content: {html}")
     print(f"\t- utf8 content: {decoded_html}")
-    
