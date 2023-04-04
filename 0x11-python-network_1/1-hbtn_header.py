21b13a0b9ef327256525523e35ee5f628ba47c7b
Oluwakemi Omoyeni <shininglite2012@gmail.com>
2023-04-04 12:20:11 +0100

    rearrabged the code

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-"""Python script that fetches https://alx-intranet.hbtn.io/status
 """
+a Python script that takes in a URL, sends a request to the URL and displays the value of
+the X-Request-Id variable found in the header of the response
+"""
+import urllib.request
+import sys
+
 
 if __name__ == "__main__":
-    import urllib.request
-    import sys
-    
     url = sys.argv[1]
     with urllib.request.urlopen(url) as response:
         url_header = response.info()
-
-    print(url_header.get('X-Request-Id'))
+        print(url_header.get('X-Request-Id'))
