4057542e6299801a2c8e1255edef8edb87adc799
Oluwakemi Omoyeni <shininglite2012@gmail.com>
2023-04-04 12:37:14 +0100

    removed line

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,7 +8,7 @@ import sys
 
 
 if __name__ == "__main__":
-    url = urllib.request.Request(sys.argv[1])
+    url = sys.argv[1]
     with urllib.request.urlopen(url) as response:
         url_header = response.info()
         print(url_header.get('X-Request-Id'))
