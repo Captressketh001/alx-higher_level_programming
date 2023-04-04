1ae6de7024d3b40a52758d94d4a4d03b552d184b
Oluwakemi Omoyeni <shininglite2012@gmail.com>
2023-04-04 16:52:29 +0100

    github authentication

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+a Python script that takes your GitHub credentials (username and
+password) and uses the GitHub API to display your id
+"""
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    url = 'https://api.github.com/user'
+    username = argv[1]
+    pwd = argv[2]
+
+    payload = {'username': username, 'password':pwd}
+    res = requests.get(url, data=payload)
+    if res.status_code == 200:
+        data = res.json()
+        print(data['id'])
