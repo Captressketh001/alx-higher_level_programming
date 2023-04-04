4f411f425744e4b76119d6ae25bd326471db51c0
Oluwakemi Omoyeni <shininglite2012@gmail.com>
2023-04-04 13:19:26 +0100

    fixed pycodestyle issue

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,10 +6,11 @@ if __name__ == "__main__":
     import urllib.request
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status')\
         as response:
-        html = response.read()
+            html = response.read()
 
     decoded_html = html.decode('utf-8')
     print("Body response:")
     print(f"\t- type: {type(html)}")
     print(f"\t- content: {html}")
     print(f"\t- utf8 content: {decoded_html}")
+    
