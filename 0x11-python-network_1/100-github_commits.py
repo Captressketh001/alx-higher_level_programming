e41b1ee217d861e1fc1c1640e94d9d19ae59dd9e
Oluwakemi Omoyeni <shininglite2012@gmail.com>
2023-04-04 17:16:20 +0100

    interview task test

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+a Python script that takes your GitHub credentials (username and
+password) and uses the GitHub API to display your id
+"""
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    url = 'https://api.github.com/user'
+    repo = argv[1]
+    owner = argv[2]
+
+    payload = {'owner': owner, 'repo': repo}
+    res = requests.get(url, params=payload)
+    if res.status_code == 200:
+        data = res.json()[:10]
+        print(data)
+    else:
+        print("None")
