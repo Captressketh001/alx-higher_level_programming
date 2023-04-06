7aabe6ca7bf3cb308b8dd76fd5a99690859c08e5
Oluwakemi Omoyeni <shininglite2012@gmail.com>
2023-04-06 08:36:42 +0100

    used sys instead of argv alone

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,11 +4,11 @@ a Python script that takes your GitHub credentials (username and
 password) and uses the GitHub API to display your id
 """
 import requests
-from sys import argv
+import sys
 
 if __name__ == '__main__':
-    repo = argv[1]
-    owner = argv[2]
+    repo = sys.argv[1]
+    owner = sys.argv[2]
     url = f'https://api.github.com/repos/{owner}/{repo}/commits'
     res = requests.get(url)
     if res.status_code == 200:
