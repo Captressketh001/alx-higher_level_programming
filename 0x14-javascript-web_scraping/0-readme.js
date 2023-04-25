3edf13eab9e1b6407a8f5932b14218d63cafad27
Captressketh001 <shininglite2012@gmail.com>
2023-04-25 21:30:27 +0000

    task 2

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,11 +2,11 @@
 // a script that reads and prints the content of a file.
 const fs = require('fs');
 const filepath = process.argv[2];
-function readContent(filepath) {
+function readContent (filepath) {
   try {
-    const content = fs.readFileSync(filepath, {encoding: 'utf-8'});
+    const content = fs.readFileSync(filepath, { encoding: 'utf-8' });
     console.log(content);
-  } catch (error){
+  } catch (error) {
     console.log(error);
   }
 }
